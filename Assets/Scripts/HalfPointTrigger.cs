28dfe575bf59b3f5cf792b764d5f801c749e9b0e
Omar Toson Khater <44278568+OmarToson@users.noreply.github.com>
2018-10-19 02:36:07 +0200

    Edit

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour {
+
+    public GameObject LabCompleteTri;
+    public GameObject HalfCompleteTri;
+
+    void OnTriggerEnter()
+    {
+        LabCompleteTri.SetActive(true);
+        HalfCompleteTri.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/LapComplete.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapComplete : MonoBehaviour {
+
+    public GameObject MinuteBest;
+    public GameObject SecondBest;
+    public GameObject MilliBest;
+
+    public GameObject LapCompleteTri;
+    public GameObject HalfLapTri;
+
+    public GameObject LapCounter;
+    private int LapDone;
+
+    public GameObject car;
+
+
+    void OnTriggerEnter()
+    {
+        LapDone += 1;
+        MilliBest.GetComponent<Text>().text = "" + LapTimeManager.MilliCount;
+
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            SecondBest.GetComponent<Text>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            SecondBest.GetComponent<Text>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+
+        if (LapTimeManager.MinuteCount <= 9)
+        {
+            MinuteBest.GetComponent<Text>().text = "0" + LapTimeManager.MinuteCount + ":";
+        }
+        else
+        {
+            MinuteBest.GetComponent<Text>().text = "" + LapTimeManager.MinuteCount + ":";
+        }
+
+        LapTimeManager.MilliCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MinuteCount = 0;
+
+        LapCounter.GetComponent<Text>().text = "" + LapDone;
+
+        if (LapDone == 2)
+        {
+            car.SetActive(false);
+        }
+
+        HalfLapTri.SetActive(true);
+        LapCompleteTri.SetActive(false);
+        
+    }
+
+}
